f177c81bf9546ca3f388ee7558c9dc0b5fb72187
E-Okelloh <eouma359@gmail.com>
2026-05-28 23:59:44 +0300

    Refreshing pyhton skills through fundermentals

---
+++ main.py
@@ -0,0 +1,17 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+    else:
+        return "You entered at negative number!"
+
+
+user_input = input(
+    "Hey user, enter the number of days and i will convert it into hours\n")
+user_input_number = int(user_input)
+
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
